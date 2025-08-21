696df704d901c0eb6ab0b3caf48812e582d98218
Junior <nilsonjosendejr@gmail.com>
2025-08-21 20:34:18 -0300

    exercise 1 to 3

---
+++ exercicio-3/multiplicacao-exercicio.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Multiplicação</title>
+    <link rel="stylesheet" href="../bootstrap-tut/bootstrap.min.css">
+</head>
+<body class="bg-dark container">
+    <h1 class="text-light text mt-4">Multiplicação</h1>
+<form method="post" action="multiplicacao-exercicio.php" class="p-4 mt-4 text-light container">
+<div class="row inline-row mb-3"><div class="col-md-2">
+              <label for="num1" class="form-label">Primeiro Número</label>
+              <input type="number" id="num1" name="num1" class="form-control" required="">
+            </div><div class="col-md-2">
+              <label for="num2" class="form-label">Segundo Número</label>
+              <input type="text" id="num2" name="num2" class="form-control" required="">
+            </div></div>
+<button type="submit" class="btn btn-primary">Enviar</button>
+</form>
+<?php
+  if($_SERVER["REQUEST_METHOD"] == "POST"){
+    $num1 = $_POST['num1'];
+    $num2 = $_POST['num2'];
+    $res = $num1 * $num2;
+    
+    echo '<div><h1 class="mt-4 text-center text-light">A res é '.$res.'</h1></div>';
+  }
+
+
+?>
+
+<script src="../bootstrap-tut/bootstrap.bundle.min.js"></script>
+</body>
+</html>
