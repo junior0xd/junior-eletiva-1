444d2985c2a6b9878aa9e829ff8cf0b36183dccf
Junior <nilsonjosendejr@gmail.com>
2025-08-21 20:28:50 -0300

    added some folders, same page sum done

--- php/form-num-1.php
+++ php/form-num-1.php
@@ -6,9 +6,9 @@
     <title>Exemplo 2</title>
     <link rel="stylesheet" href="../bootstrap-tut/bootstrap.min.css">
 </head>
-<body class="bg-dark container">danger
+<body class="bg-dark container">
     <h1 class="text-light text mt-4">Exemplo 2</h1>
-<form method="post" action="resposta-num-1.php" class="p-4 mt-4 text-light container">
+<form method="post" action="form-num-1.php" class="p-4 mt-4 text-light container">
 <div class="row inline-row mb-3"><div class="col-md-2">
               <label for="num1" class="form-label">Primeiro Número</label>
               <input type="number" id="num1" name="num1" class="form-control" required="">
@@ -18,6 +18,18 @@
             </div></div>
 <button type="submit" class="btn btn-primary">Enviar</button>
 </form>
+<?php
+  if($_SERVER["REQUEST_METHOD"] == "POST"){
+    $num1 = $_POST['num1'];
+    $num2 = $_POST['num2'];
+    $soma = $num1 + $num2;
+    
+    echo '<div><h1 class="mt-4 text-center text-light">A soma é '.$soma.'</h1></div>';
+  }
+
+
+?>
+
 <script src="../bootstrap-tut/bootstrap.bundle.min.js"></script>
 </body>
 </html>
