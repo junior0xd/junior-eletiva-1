d4c62a5f2d8807e703589e257d395fac8dd344cb
Junior <nilsonjosendejr@gmail.com>
2025-08-22 10:44:31 -0300

    exercise 6

--- exercicio-6/conversao-temp.php
+++ exercicio-6/conversao-temp.php
@@ -10,16 +10,28 @@
 
 <body class="bg-dark text-light">
     <main class="container m-4">
-    <form action="conversao-temp" method="post" class="row justify-content-center">
+    <form action="conversao-temp.php" method="post" class="row justify-content-center">
         <h1 class="mb-5">Converter Temperatura</h1>
-        <div class="col-2">
-            <div class="input-group mb-3 col-12">
-                <input type="number" step="0.01" class="form-control" placeholder="T" aria-label="Temperatura" aria-describedby="basic-addon1">
+        <div class="col-12">
+            <div class="input-group mb-3 w-25">
+                <input name="temperatura" type="number" step="0.01" class="form-control" placeholder="T" aria-label="Temperatura" aria-describedby="basic-addon1">
                 <span class="input-group-text" id="basic-addon1">°C</span>
             </div>
         </div>
-        <button type="submit" class="btn btn-primary col-1">Enviar</button>
+        <div class="row">
+            <button type="submit" class="btn btn-primary col-1">Enviar</button>
+        </div>
+        
     </form>
+    <?php 
+    if($_SERVER["REQUEST_METHOD"] == "POST"){
+        $t_celsius = $_POST["temperatura"];
+        $fahrenheit = ($t_celsius * 1.8) + 32;
+
+        echo '<h1 class="text-light"> A temperatura em fahrenheit é '.number_format($fahrenheit, 1).'</h1>';
+    }
+    
+    ?>
     </main>
     <script src="../bootstrap-tut/bootstrap.bundle.min.js"></script>
 </body>
