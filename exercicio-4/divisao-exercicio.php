fa2e11fbefe9a006786b12514f22d3b6efc174d3
Junior <nilsonjosendejr@gmail.com>
2025-08-21 21:16:13 -0300

    exercise 4

--- exercicio-4/divisao-exercicio.php
+++ exercicio-4/divisao-exercicio.php
@@ -3,11 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Multiplicação</title>
+    <title>Divisão</title>
     <link rel="stylesheet" href="../bootstrap-tut/bootstrap.min.css">
 </head>
 <body class="bg-dark container">
-    <h1 class="text-light text mt-4">Multiplicação</h1>
+    <h1 class="text-light text mt-4">Divisão</h1>
 <form method="post" action="divisao-exercicio.php" class="p-4 mt-4 text-light container">
 <div class="row inline-row mb-3"><div class="col-md-2">
               <label for="num1" class="form-label">Primeiro Número</label>
@@ -16,16 +16,20 @@
               <label for="num2" class="form-label">Segundo Número</label>
               <input type="text" id="num2" name="num2" class="form-control" required="">
             </div></div>
+            <p>Digite um número e depois outro para dividir pelo primeiro.</p>
 <button type="submit" class="btn btn-primary">Enviar</button>
 </form>
 <?php
   if($_SERVER["REQUEST_METHOD"] == "POST"){
     $num1 = $_POST['num1'];
     $num2 = $_POST['num2'];
-    $res = $num1 / $num2;
-    if ($num1 )
+    if ($num2 == 0){
+      echo '<div><h1 class="mt-4 text-center text-light"> Hmmm... não é possível dividir por zero</h1></div>';
+    } else {
+      $res = $num1 / $num2;
+      echo '<div><h1 class="mt-4 text-center text-light">O resultado é igual a '.$res.'</h1></div>';
+    }
     
-    echo '<div><h1 class="mt-4 text-center text-light">A res é '.$res.'</h1></div>';
   }
 
 
