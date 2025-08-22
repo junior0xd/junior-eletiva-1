d3198330cee27879b2330ea1da95129b898dda34
Junior <nilsonjosendejr@gmail.com>
2025-08-22 10:52:42 -0300

    exercise 7

--- exercicio-7/conversao-temp.php
+++ exercicio-7/conversao-temp.php
@@ -15,7 +15,7 @@
         <div class="col-12">
             <div class="input-group mb-3 w-25">
                 <input name="temperatura" type="number" step="0.01" class="form-control" placeholder="T" aria-label="Temperatura" aria-describedby="basic-addon1">
-                <span class="input-group-text" id="basic-addon1">°C</span>
+                <span class="input-group-text" id="basic-addon1">°F</span>
             </div>
         </div>
         <div class="row">
@@ -25,10 +25,10 @@
     </form>
     <?php 
     if($_SERVER["REQUEST_METHOD"] == "POST"){
-        $t_celsius = $_POST["temperatura"];
-        $fahrenheit = ($t_celsius * 1.8) + 32;
+        $t_fahrenheit = $_POST["temperatura"];
+        $celsius = ($t_fahrenheit - 32 ) / 1.8;
 
-        echo '<h1 class="text-light mt-4"> A temperatura em fahrenheit é '.number_format($fahrenheit, 1).'</h1>';
+        echo '<h1 class="text-light mt-4"> A temperatura em celsius é '.number_format($celsius, 1).'</h1>';
     }
     
     ?>
