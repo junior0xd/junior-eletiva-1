53c7604e7935666de2c892c69e4d8f4265675a06
Junior <nilsonjosendejr@gmail.com>
2025-08-28 21:36:29 -0300

    exercise 13

--- exercicio-13/converter-centimetros.php
+++ exercicio-13/converter-centimetros.php
@@ -10,16 +10,12 @@
 
 <body class="bg-dark text-light">
     <main class="container mt-5">
-        <h1 class="text-light text-center">Converter metros em</h1>
-        <form class="border rounded-2 border-light-subtle" action="calcular-expoente.php" method="post">
+        <h1 class="text-light text-center">Converter metros em centímetros</h1>
+        <form class="border rounded-2 border-light-subtle" action="converter-centimetros.php" method="post">
             <div class="row flex justify-content-center m-4">
                 <div class="col-2">
-                    <label class="form-label" for="base">Base</label>
-                    <input class="form-control" type="number" step="0.01" id="base" name="base">
-                </div>
-                <div class="col-2">
-                    <label class="form-label" for="expoente">Expoente</label>
-                    <input class="form-control" type="number" step="0.01" id="expoente" name="expoente">
+                    <label class="form-label" for="metros">Metros</label>
+                    <input class="form-control" type="number" step="0.01" id="metros" name="metros">
                 </div>
                 <div class="row m-4 flex justify-content-center">
                     <button type="submit" class="btn btn-primary w-25">Enviar</button>
@@ -27,15 +23,14 @@
         </form>
     </main>
     <?php
-        if ($_SERVER["REQUEST_METHOD"] == "POST") {
-            $base = $_POST["base"];
-            $expoente = $_POST["expoente"];
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $metros = $_POST["metros"];
 
-            $resultado = $base ** $expoente;
+        $centimetros = $metros * 100;
 
-            echo '<h1 class="text-light text-center mt-3">O resultado é igual a ' . $resultado . '</h1>';
-        }
-        ?>
+        echo '<h1 class="text-light text-center mt-3">' . $metros . ' metros é igual a ' . $centimetros . ' centímetros</h1>';
+    }
+    ?>
     <script src="../bootstrap-tut/bootstrap.min.css"></script>
 </body>
 
