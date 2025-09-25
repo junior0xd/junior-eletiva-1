236a9e9c46153372b109b41c3f130d27fe5785db
Junior <nilson.josende@fatec.sp.gov.br>
2025-09-24 21:36:48 -0300

    iniciando lista 2, exemplos de cabeçalho

---
+++ lista-2/cabecalho.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../bootstrap.min.css">
+    <title>Lista de Exercícios 2</title>
+</head>
+
+<body class="bg-dark text-light container">
+    <nav class="navbar navbar-expand-lg mb-4 mt-4 bg-light rounded-2">
+        <div class="container">
+            <a class="navbar-brand" href="#">[GOATSE]</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Alternar navegação">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="#">Início</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="#">Exercício</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="#">Sobre</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
