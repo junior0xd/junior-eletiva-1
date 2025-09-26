b43078a59f4d83b79c3d292e969c978d4b6b77f1
Junior <nilsonjosendejr@gmail.com>
2025-09-25 22:10:12 -0300

    projeto exemplo

--- projeto/conexao.php
+++ projeto/conexao.php
@@ -1,11 +1,13 @@
 <?php 
-$host = "localhost";
-$user = "bisel";
-$pass = "junior";
+$host = "mysql:host=localhost;dbname=projetophp";
+$user = "root";
+$pass = "";
 
 try {
     $pdo = new PDO($host, $user, $pass);
 } catch (Exception $e) {
     die("Erro:".$e->getMessage());
 }
+
+//FALTA TESTAR
 ?>
