cd03394ed6a112cc1495418b356062949ba2b508
Junior <nilson.josende@fatec.sp.gov.br>
2025-09-27 16:47:36 -0300

    gibberish

---
+++ lista-3/cabecalho.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../bootstrap.min.css">
+    <title>Lista de Exercícios 2</title>
+</head>
+
+<body class="bg-dark text-light container">
+    <nav class="navbar navbar-expand-lg mb-4 mt-4 bg-light rounded-2">
+        <div class="container">
+            <a class="navbar-brand border border-2 rounded-2 p-2 border-primary" href="#">[GOATSE]</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Alternar navegação">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="main.php">Início</a>
+                    </li>
+
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown">
+                            Exercícios
+                        </a>
+                        <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="?page=exercicio-1">Exercício 1</a></li>
+                                <li><a class="dropdown-item" href="?page=exercicio-2">Exercício 2</a></li>
+                                <li><a class="dropdown-item" href="?page=exercicio-3">Exercício 3</a></li>
+                                <li><a class="dropdown-item" href="?page=exercicio-4">Exercício 4</a></li>
+                                <li><a class="dropdown-item" href="?page=exercicio-5">Exercício 5</a></li>
+                                <li><a class="dropdown-item" href="?page=exercicio-6">Exercício 6</a></li>
+                                <li><a class="dropdown-item" href="?page=exercicio-7">Exercício 7</a></li>
+                                <li><a class="dropdown-item" href="?page=exercicio-8">Exercício 8</a></li>
+                                <li><a class="dropdown-item" href="?page=exercicio-9">Exercício 9</a></li>
+                                <li><a class="dropdown-item" href="?page=exercicio-10">Exercício 10</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="#sobre">Sobre</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
---
+++ lista-3/gangstas-paradise.php
@@ -0,0 +1,69 @@
+<div class="container mt-5 text-center">
+    <h1 class="fs-2"> Gangsta's Paradise</h1>
+    <h2 class="fs-4"> Coolio</h2>
+    <p class="fs-6">As I walk through the valley of the shadow of death
+        <br> I take a look at my life and realize there's nothin' left
+        <br> 'Cause I've been blastin' and laughin' so long that
+        <br> Even my mama thinks that my mind is gone
+    </p>
+    <p class="fs-6">But I ain't cross a man that didn't deserve it
+        <br> Me be treated like a punk, you know that's unheard of
+        <br> You better watch how you're talkin' and where you're walkin'
+        <br> Or you and your homies might be lined in chalk
+    </p>
+    <p class="fs-6">I really hate to trip but I gotta loc
+        <br> As they croak, I see myself in the pistol smoke, fool
+        <br> I'm the kinda G that little homies wanna be like
+        <br> On my knees in the night, sayin' prayers in the street light
+    </p>
+    <p class="fs-6">Been spendin' most their lives
+        <br> Livin' in the gangsta's paradise (x4)
+    </p>
+
+    <p class="fs-6"> Look at the situation they got me facin'
+        <br> I can't live a normal life, I was raised by the state
+        <br> So I gotta be down with the hood team
+        <br> Too much television watchin' got me chasin' dreams
+    </p>
+
+    <p class="fs-6"> I'm an educated fool with money on my mind
+        <br> Got my ten in my hand and a gleam in my eye
+        <br> I'm a loc'd out gangsta, set trippin' banger
+        <br> And my homies is down so don't arouse my anger
+    </p>
+
+    <p class="fs-6">Fool, Death ain't nothin' but a heartbeat away
+        <br> I'm livin' life do or die, what can I say?
+        <br> I'm twenty-three now, but will I live to see twenty-four?
+        <br> The way things is goin' I don't know
+    </p>
+
+    <p class="fs-6"> Tell me why are we so blind to see
+        <br> That the ones we hurt are you and me?
+    </p>
+
+    <p class="fs-6">Been spendin' most their lives
+        <br> Livin' in the gangsta's paradise (x4)
+    </p>
+
+    <p class="fs-6"> Power and the money, money and the power
+        <br> Minute after minute, hour after hour
+        <br> Everybody's runnin', but half of them ain't lookin'
+        <br> It's goin' on in the kitchen, but I don't know what's cookin'
+    </p>
+
+    <p class="fs-6"> They say I gotta learn, but nobody's here to teach me
+        <br> If they can't understand it, how can they reach me?
+        <br> I guess they can't, I guess they won't
+        <br> I guess they front, that's why I know my life is out of luck, fool
+    </p>
+    
+    <p class="fs-6">Been spendin' most their lives
+        <br> Livin' in the gangsta's paradise (x4)
+    </p>
+
+    <p class="fs-6"> Tell me why are we so blind to see
+        <br> That the ones we hurt are you and me? (x2)
+    </p>
+<div>
+        
