03fa2c9826f5213131f876e94e05a13a4a92a36a
Junior <nilson.josende@fatec.sp.gov.br>
2025-09-28 18:55:44 -0300

    exercicio 2 lista 3

--- lista-3/exercicio-2.php
+++ lista-3/exercicio-2.php
@@ -1,3 +1,23 @@
 <div class="container mt-4">
-    <h1></h1>
+    <h1 class="fs-4 text-center">Exercício 2 - Lista 3</h1>
+    <form action="?page=exercicio-2" method="post">
+        <div class="row justify-content-center">
+            <div class="col-6">
+                <label for="form-label" for="texto"> Texto</label>
+                <input class="form-control" type="text" id="texto" name="texto" required>
+            </div>
+            <div class="row mt-4 mb-4 justify-content-center">
+                <button type="submit" class="btn btn-primary w-25">Enviar</button>
+            </div>
+        </div>
+    </form>
+    <?php if ($_SERVER["REQUEST_METHOD"] == "POST"){
+        $texto = $_POST["texto"];
+        $minusculo = strtolower($texto);
+        $maiusculo = strtoupper($texto);
+
+        echo '<h1 class="text-light text-center mt-3">Texto em minúsculo: ' . htmlspecialchars($minusculo) . '</h1>';
+        echo '<h1 class="text-light text-center mt-3">Texto em minúsculo: ' . htmlspecialchars($maiusculo) . '</h1>';
+    }
+    ?>
 </div>
